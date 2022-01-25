5464c3850acb85e65fe4243ca79ccb52a32f8a08
Adam Kim <adam@invoicemaker.com>
2022-01-25 17:24:07 -0500

    README

--- .svelte-kit/generated/manifest.js
+++ .svelte-kit/generated/manifest.js
@@ -4,7 +4,6 @@ const c = [
 	() => import("../../src/routes/index.svelte"),
 	() => import("../../src/routes/experiences/index.svelte"),
 	() => import("../../src/routes/projects/index.svelte"),
-	() => import("../../src/routes/todos/index.svelte"),
 	() => import("../../src/routes/etc/index.svelte")
 ];
 
@@ -20,11 +19,8 @@ export const routes = [
 	// src/routes/projects/index.svelte
 	[/^\/projects\/?$/, [c[0], c[4]], [c[1]]],
 
-	// src/routes/todos/index.svelte
-	[/^\/todos\/?$/, [c[0], c[5]], [c[1]]],
-
 	// src/routes/etc/index.svelte
-	[/^\/etc\/?$/, [c[0], c[6]], [c[1]]]
+	[/^\/etc\/?$/, [c[0], c[5]], [c[1]]]
 ];
 
 // we import the root layout/error components eagerly, so that
